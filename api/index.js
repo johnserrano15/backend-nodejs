e5515eb47716108ef3fee3e3032840be37e82b5a
John Andrey <andrey_201015@hotmail.com>
2020-07-29 10:44:47 -0500

    Gestión avanzada de errores: Throw

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const swaggerUi = require('swagger-ui-express')
 const config = require('../config')
 const user = require('./components/user/network')
 const auth = require('./components/auth/network')
+const errors = require('../network/errors')
 
 const app = express()
 
@@ -16,6 +17,8 @@ const swaggerDocument = require('./swagger.json')
 app.use('/api/user', user)
 app.use('/api/auth', auth)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.use(errors)
 
 app.listen(config.api.port, () => {
   console.log(`Api escuchando http://localhost:${config.api.port}`, )
--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const config = require('../config')
+const error = require('../utils/error')
 
 const secret = config.jwt.secret
 
@@ -18,18 +19,18 @@ const check = {
     console.log(owner)
 
     if (decoded.id !== owner) {
-      throw new Error('No puedes hacer esto');
+      throw error('No puedes hacer esto', 401)
     }
   },
 }
 
 function getToken(auth) {
   if (!auth) {
-    throw new Error('No viene token')
+    throw error('No viene token', 401)
   }
 
   if (auth.indexOf('Bearer ') === -1) {
-    throw new Error('Formato invalido')
+    throw error('Formato invalido', 400)
   }
 
   let token = auth.replace('Bearer ', '')
